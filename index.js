09351afa9d5f98d776a42af2bcf339a72e08b30c
quan005 <ralphealfreeman@gmail.com>
2019-08-13 19:09:36 -0700

    configured index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code away!
+// Import Express
+const express = require("express");
+
+ // Routes
+const userRoutes = require("./routes/userRoutes.js");
+const postRoutes = require("./routes/postRoutes.js");
+
+ // Initialize Express
+const server = express();
+
+ // Middleware
+server.use(express.json());
+
+ // Use Routes
+server.use("/api/users", userRoutes);
+server.use("/api/posts", postRoutes);
+
+ // Run Server
+server.listen(4000, () => {
+  console.log("server running");
+});
